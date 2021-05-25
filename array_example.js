b71e255cc71b5fc3d05a32caec92a9ad0c035805
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-05-25 12:32:03 -0500

    JS updates

---
+++ array_example.js
@@ -0,0 +1,23 @@
+    // alert( "Hello, world!" );
+
+    // Create an array of Movie monsters
+const KAIJU = [
+    { name: 'Godzilla', tier: 1 },
+    { name: 'King Kong', tier: 2 },
+    { name: 'Gamera', tier: 1 },
+    { name: 'Mothra', tier: 4 },
+    { name: 'Ghidora', tier: 4 },
+    { name: 'Ultraman', tier: 1 },
+    { name: 'Rodan', tier: 4 },
+    ]
+
+var monsterVariable = KAIJU((monster) => {
+    //console.log(monster.name)
+     push(monster.name);
+    //console.log(session)
+    }
+)
+
+
+
+  
